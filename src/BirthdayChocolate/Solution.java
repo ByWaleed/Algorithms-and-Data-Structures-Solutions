81f5a4847b675739c635ad9e40327cfeab354b94
Waleed <waleedi825@gmail.com>
2019-10-16 23:11:42 +0100

    Birthday Chocolate solution added

---
+++ src/BirthdayChocolate/Solution.java
@@ -0,0 +1,64 @@
+/*
+  Problem: https://www.hackerrank.com/challenges/the-birthday-bar/problem
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the birthday function below.
+    static int birthday(List<Integer> s, int d, int m) {
+        int portions = 0;
+
+        for(int i=0; i < s.size(); i++){
+            int sum = 0;
+
+            for(int j = 0 ; (j < m) && ((j + i) < s.size()); j++){
+                    if(sum > d){
+                        break;
+                    }else{
+                        sum += s.get(i+j);
+                    }
+            }
+            if(sum == d){
+                portions++;
+            }
+        }
+        return portions;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String[] dm = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(dm[0]);
+
+        int m = Integer.parseInt(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
