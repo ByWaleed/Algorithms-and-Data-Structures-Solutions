4f90939aed11d0aadf93ef7a707f01b895ffbe40
ByWaleed <waleedi825@gmail.com>
2020-10-11 14:34:57 +0100

    Google KickStart 2020: Rouad A Question 1 implemented

---
+++ src/GoogleKickStart/Year2020/Question1/Solution.java
@@ -0,0 +1,36 @@
+package GoogleKickStart.Year2020.Question1;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    private static int solve(int budget, int[] prices) {
+        if(prices.length == 0) return 0;
+        Arrays.sort(prices);
+        int n = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (budget >= prices[i]) {
+                n++;
+                budget -= prices[i];
+            } else return n;
+        }
+
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        int t = in.nextInt(); // Scanner has functions to read ints, longs, strings, chars, etc.
+
+        for (int i = 1; i <= t; ++i) {
+            int houses = in.nextInt();
+            int budget = in.nextInt();
+            int[] prices = new int[houses];
+
+            for (int h = 0; h < houses; h++) prices[h] = in.nextInt();
+
+            System.out.println("Case #" + i + ": " +  solve(budget, prices));
+        }
+    }
+}
