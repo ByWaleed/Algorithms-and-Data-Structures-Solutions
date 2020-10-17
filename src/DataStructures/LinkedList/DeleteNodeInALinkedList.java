a546ad4eb691d6bf30a417cf24a9a6cf0a5885a9
ByWaleed <waleedi825@gmail.com>
2020-10-17 20:44:49 +0100

    LinkedList: delete node is a list (only node to delete provided)

---
+++ src/DataStructures/LinkedList/DeleteNodeInALinkedList.java
@@ -0,0 +1,20 @@
+/**
+ * Name: Delete Node in a Linked List
+ * Platform: LeetCode
+ * URL: https://leetcode.com/problems/delete-node-in-a-linked-list/
+ * */
+package DataStructures.LinkedList;
+
+public class DeleteNodeInALinkedList {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    private void DeleteNodeInALinkedList(ListNode node) {
+        node.val = node.next.val; // copy next node, to current node
+        node.next = node.next.next; // remove next node
+    }
+}
